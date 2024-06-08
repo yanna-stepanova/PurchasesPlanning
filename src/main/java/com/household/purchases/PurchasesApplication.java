b2ff6fcfbe99a1b6bf62480becb525655f5a62a6
Yana Stepanova <yanna.stepanova@gmail.com>
2024-06-08 17:44:09 +0300

    init project

---
+++ src/main/java/com/household/purchases/PurchasesApplication.java
@@ -0,0 +1,13 @@
+package com.household.purchases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/household/purchases/PurchasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.household.purchases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
